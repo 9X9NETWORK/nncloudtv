b9b51537dae6f9f65482ae2c0e6def82e3c0de3d
ywntseng <yiwen.teltel@gmail.com>
2014-01-09 02:45:02 +0000

    playerapi json

--- src/main/java/com/nncloudtv/service/NnGuestManager.java
+++ src/main/java/com/nncloudtv/service/NnGuestManager.java
@@ -1,11 +1,13 @@
-package com.nncloudtv.service;
 
+package com.nncloudtv.service;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.nncloudtv.dao.NnGuestDao;
 import com.nncloudtv.model.NnGuest;
+import com.nncloudtv.model.NnUser;
+import com.nncloudtv.web.json.player.UserInfo;
 
 public class NnGuestManager {
 
@@ -27,5 +29,24 @@ public class NnGuestManager {
     
     public NnGuest findByToken(String token) {
         return guestDao.findByToken(token);
+    }
+    
+    public Object getPlayerGuestRegister(NnGuest guest, short format, HttpServletRequest req) {
+        String sphere = NnUserManager.findLocaleByHttpRequest(req);
+    	if (format == PlayerApiService.FORMAT_PLAIN) {
+            String[] result = {""};
+            result[0] += PlayerApiService.assembleKeyValue("token", guest.getToken());
+            result[0] += PlayerApiService.assembleKeyValue("name", NnUser.GUEST_NAME);
+            result[0] += PlayerApiService.assembleKeyValue("sphere", sphere);
+            result[0] += PlayerApiService.assembleKeyValue("lastLogin", "");    	
+            return result;
+    	} else {
+    		UserInfo json = new UserInfo();
+    		json.setToken(guest.getToken());
+            json.setName(NnUser.GUEST_NAME);
+            json.setSphere(sphere);
+            json.setLastLogin("");
+            return json;
+    	}    	
     }
 }
