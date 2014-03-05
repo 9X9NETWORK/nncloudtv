7219132388c8c4a4a67739b9092668232e45db45
Louis Jeng <louisje@gmail.com>
2014-03-05 13:52:21 +0800

    update schema

--- mysql/schema/nncloudtv_content.sql
+++ mysql/schema/nncloudtv_content.sql
@@ -560,6 +560,27 @@ CREATE TABLE `ytprogram` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+--
+-- Table structure for table `mso_notification`
+--
+
+DROP TABLE IF EXISTS `mso_notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mso_notification` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `message` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `msoid` bigint(20) NOT NULL,
+  `publishdate` datetime DEFAULT NULL,
+  `scheduledate` datetime DEFAULT NULL,
+  `updatedate` datetime DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
