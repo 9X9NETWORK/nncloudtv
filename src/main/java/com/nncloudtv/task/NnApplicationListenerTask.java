250fee3c25fbb2c6ea097f11d3eaee78abe922ea
Louis Jeng <louisje@gmail.com>
2014-05-30 11:32:46 +0800

    task:annotation-driven

--- src/main/java/com/nncloudtv/task/NnApplicationListenerTask.java
+++ src/main/java/com/nncloudtv/task/NnApplicationListenerTask.java
@@ -15,6 +15,8 @@ public class NnApplicationListenerTask implements ApplicationListener<ContextRef
         
         log.info("EVENT: " + event.toString());
         
+        
+        
     }
     
 }
