541cc2b61cc22b0daa48b30e914d8f784dd61b37
Louis Jeng <louisje@gmail.com>
2014-05-30 05:33:24 +0000

    enable scheduling task

--- src/main/java/com/nncloudtv/task/ScheduledTask.java
+++ src/main/java/com/nncloudtv/task/ScheduledTask.java
@@ -2,15 +2,18 @@ package com.nncloudtv.task;
 
 import java.util.logging.Logger;
 
+import org.springframework.stereotype.Service;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@Service
 @EnableScheduling
 public class ScheduledTask {
     
     protected static Logger log = Logger.getLogger(ScheduledTask.class.getName());
     
-    @Scheduled(fixedDelay = 100)
+    @Scheduled(fixedDelay = 10000)
     public void checkingMemcacheServer() {
         
         log.info("Checking memcache server");
