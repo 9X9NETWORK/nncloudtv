4d9569af3fa5b01dee4f77d980a53738cdb64ff0
Louis Jeng <louisje@gmail.com>
2014-05-30 10:36:03 +0800

    ApplicationListener

---
+++ src/main/java/com/nncloudtv/task/NnApplicationListenerTask.java
@@ -0,0 +1,24 @@
+package com.nncloudtv.task;
+
+import java.util.logging.Logger;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NnApplicationListenerTask implements ApplicationListener<ApplicationEvent> {
+    
+    protected static Logger log = Logger.getLogger(NnApplicationListenerTask.class.getName());
+    
+    public void onApplicationEvent(ApplicationEvent event) {
+        
+        if (event instanceof ContextStartedEvent) {
+            
+            log.info("Application started!");
+        }
+        
+    }
+    
+}
