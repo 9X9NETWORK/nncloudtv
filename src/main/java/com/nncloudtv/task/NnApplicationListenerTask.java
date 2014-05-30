a47ac56be5a96bf938033cd68d957552943588ce
Louis Jeng <louisje@gmail.com>
2014-05-30 11:07:20 +0800

    ContextEventListener

--- src/main/java/com/nncloudtv/task/NnApplicationListenerTask.java
+++ src/main/java/com/nncloudtv/task/NnApplicationListenerTask.java
@@ -2,24 +2,18 @@ package com.nncloudtv.task;
 
 import java.util.logging.Logger;
 
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NnApplicationListenerTask implements ApplicationListener<ApplicationEvent> {
+public class NnApplicationListenerTask implements ApplicationListener<ContextRefreshedEvent> {
     
     protected static Logger log = Logger.getLogger(NnApplicationListenerTask.class.getName());
     
-    public void onApplicationEvent(ApplicationEvent event) {
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         
-        log.info("EVENT" + event.toString());
-        
-        if (event instanceof ContextStartedEvent) {
-            
-            log.info("Application started!");
-        }
+        log.info("EVENT: " + event.toString());
         
     }
     
