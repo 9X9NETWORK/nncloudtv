c3c591b789b2f9434862add9aefc61d17486e779
Louis Jeng <louisje@gmail.com>
2014-06-12 20:46:43 +0800

    update table schema

--- mysql/create_databases.sql
+++ mysql/create_databases.sql
@@ -2,3 +2,4 @@ create database nncloudtv_analytics character set = utf8;
 create database nncloudtv_content character set = utf8;
 create database nncloudtv_nnuser1 character set = utf8;
 create database nncloudtv_nnuser2 character set = utf8;
+create database nncloudtv_billing character set = utf8;
