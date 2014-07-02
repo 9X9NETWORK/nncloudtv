81dbb5fc9c787cf01145767fe016c7b39c3879d5
Louis Jeng <louisje@gmail.com>
2014-07-02 16:19:24 +0800

    enable websocket

--- src/main/java/com/nncloudtv/websocket/WebSocketConfig.java
+++ src/main/java/com/nncloudtv/websocket/WebSocketConfig.java
@@ -1,12 +1,12 @@
 package com.nncloudtv.websocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurationSupport;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocket
 public class WebSocketConfig extends WebSocketConfigurationSupport {
     
     @Override
