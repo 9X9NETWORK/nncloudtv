aef9d9b87de4e3c770d283ebf5c7c094662dd837
Louis Jeng <louisje@gmail.com>
2014-07-02 16:54:41 +0800

    JettyRequestUpgradeStrategy

--- src/main/java/com/nncloudtv/websocket/WebSocketConfig.java
+++ src/main/java/com/nncloudtv/websocket/WebSocketConfig.java
@@ -2,17 +2,24 @@ package com.nncloudtv.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurationSupport;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig extends WebSocketConfigurationSupport {
+public class WebSocketConfig implements WebSocketConfigurer {
     
-    @Override
-    protected void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         
         registry.addHandler(new NnSocketHandler(), "/websocket")
-                .addInterceptors(new HandshakeInterceptor());
+                .addInterceptors(new HandshakeInterceptor())
+                .setHandshakeHandler(handshakeHandler());
+    }
+    
+    public DefaultHandshakeHandler handshakeHandler() {
+        
+        return new DefaultHandshakeHandler(new JettyRequestUpgradeStrategy());
     }
 }
