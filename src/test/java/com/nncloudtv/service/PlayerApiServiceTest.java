0661e0d584f909dc400ddc2c00137ba1e95d5467
barton <bartonfor9x9@gmail.com>
2014-07-04 18:31:23 +0800

    rewrite test cases

--- src/test/java/com/nncloudtv/service/PlayerApiServiceTest.java
+++ src/test/java/com/nncloudtv/service/PlayerApiServiceTest.java
@@ -2,11 +2,14 @@ package com.nncloudtv.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.anyBoolean;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.anyObject;
 import static org.mockito.Mockito.anyShort;
 import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -14,6 +17,8 @@ import static org.mockito.Mockito.when;
 
 import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 //import junit.framework.Assert; The old method (of Junit 3),
@@ -25,18 +30,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import com.nncloudtv.lib.CacheFactory;
+import com.nncloudtv.lib.CookieHelper;
 import com.nncloudtv.model.Mso;
+import com.nncloudtv.model.NnGuest;
 import com.nncloudtv.model.NnUser;
 import com.nncloudtv.model.NnUserProfile;
+import com.nncloudtv.validation.BasicValidator;
 import com.nncloudtv.web.api.ApiContext;
 import com.nncloudtv.web.api.NnStatusCode;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({BasicValidator.class})
 public class PlayerApiServiceTest {
 
     protected static final Logger log = Logger.getLogger(PlayerApiServiceTest.class.getName());
@@ -154,18 +165,67 @@ public class PlayerApiServiceTest {
         
         assertNotNull(actual);
     }
-    /*
-    @Test
-    public void testLogin() {
-        String email = "a@a.com";
-        String password = "123456";
-        Object loginObj = service.login(email, password, req, resp);
-        
-        assertNotNull(loginObj);
-    }
     
     @Test
-    public void testQuickLogin() {
+    public void testLogin() {
+        
+        // input arguments
+        final String email = "a@a.com";
+        final String password = "123456";
+        
+        // mock data
+        final Long msoId = (long) 1;
+        Mso mso = new Mso("name", "intro", "contactEmail", Mso.TYPE_MSO);
+        mso.setId(msoId);
+        
+        final Long userId = (long) 1;
+        final String token = "token";
+        NnUser user = new NnUser(email, password, NnUser.TYPE_USER);
+        user.setId(userId);
+        user.setToken(token);
+        
+        Object userInfo = "userInfo";
+        Object result = "result";
+        
+        // stubs
+        when(mockMsoMngr.getByNameFromCache(anyString())).thenReturn(mso);
+        
+        PowerMockito.mockStatic(BasicValidator.class);
+        when(BasicValidator.validateRequired((String[]) anyObject())).thenReturn(true);
+        
+        when(mockUserMngr.findAuthenticatedUser(anyString(), anyString(), anyLong(),
+                (HttpServletRequest) anyObject())).thenReturn(user);
+        // Spy object call one time for when() seems OK, but over two times got trouble (don't know why),
+        // call doXXXX() family method first then when() method second solve it.
+        doReturn(userInfo).when(service).prepareUserInfo((NnUser) anyObject(), (NnGuest) anyObject(),
+                (HttpServletRequest) anyObject(), anyBoolean());
+        when(mockUserMngr.save((NnUser) anyObject())).thenReturn(user);
+        doNothing().when(service).setUserCookie((HttpServletResponse) anyObject(), anyString(), anyString());
+        when(service.assembleMsgs(anyInt(), (String[]) anyObject())).thenReturn(result);
+        
+        // execute
+        service.prepService(req, resp);
+        Object actual = service.login(email, password, req, resp);
+        
+        // verify
+        verify(mockMsoMngr).getByNameFromCache(anyString());
+        
+        PowerMockito.verifyStatic();
+        BasicValidator.validateRequired(new String[] {email, password});
+        
+        verify(mockUserMngr).findAuthenticatedUser(email, password, msoId, req);
+        verify(service).prepareUserInfo(user, null, req, true);
+        verify(mockUserMngr).save(user);
+        verify(service).setUserCookie(resp, CookieHelper.USER, token);
+        
+        String[] raw = {(String) userInfo};
+        verify(service).assembleMsgs(NnStatusCode.SUCCESS, raw);
+        
+        assertEquals(result, actual);
+    }
+    /*
+    @Test
+    //public void testQuickLogin() {
         String email = "a@a.com";
         String password = "123456";        
         Object userInfo = service.login(email, password, req, resp);
