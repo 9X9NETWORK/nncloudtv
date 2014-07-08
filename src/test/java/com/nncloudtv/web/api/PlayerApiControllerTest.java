db2ab97a9a328c6cc1a7e83af63a6e98498c1c45
barton <bartonfor9x9@gmail.com>
2014-07-08 18:24:58 +0800

    replace mocks

--- src/test/java/com/nncloudtv/web/api/PlayerApiControllerTest.java
+++ src/test/java/com/nncloudtv/web/api/PlayerApiControllerTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -16,16 +20,25 @@ import java.util.logging.Logger;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import com.nncloudtv.lib.CacheFactory;
 import com.nncloudtv.lib.FacebookLib;
 import com.nncloudtv.lib.NnStringUtil;
-import com.nncloudtv.mock.service.MockMsoConfigManager;
-import com.nncloudtv.mock.service.MockMsoManager;
+import com.nncloudtv.model.Mso;
+import com.nncloudtv.service.MsoConfigManager;
+import com.nncloudtv.service.MsoManager;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({MsoManager.class,FacebookLib.class})
 public class PlayerApiControllerTest {
     
     protected static final Logger log = Logger.getLogger(PlayerApiControllerTest.class.getName());
@@ -33,29 +46,70 @@ public class PlayerApiControllerTest {
     private PlayerApiController playerAPI;
     
     private MockHttpServletRequest req;
-    private MockMsoManager mockMsoMngr;
-    private MockMsoConfigManager mockConfigMngr;
+    @Mock private MsoManager mockMsoMngr;
+    @Mock private MsoConfigManager mockConfigMngr;
     
     @Before
     public void setUp() {
         
         CacheFactory.isEnabled = false;
         
-        mockConfigMngr = new MockMsoConfigManager();
-        mockMsoMngr = new MockMsoManager();
         req = new MockHttpServletRequest();
         
         playerAPI = new PlayerApiController(mockMsoMngr);
         FacebookLib.setConfigMngr(mockConfigMngr);
     }
     
+    @After
+    public void tearDown() {
+        req = null;
+        mockMsoMngr = null;
+        mockConfigMngr = null;
+        
+        playerAPI = null;
+    }
+    
     @Test
     public void testFbLogin() {
         
-        String referrer = "http://www.mock.com/signin";
+        // input arguments
+        final String referrer = "http://www.mock.com/signin";
         req.setRequestURI("/playerAPI/fbLogin");
         req.addHeader(ApiContext.HEADER_REFERRER, referrer);
+        
+        // mock data
+        final Long msoId = (long) 1;
+        Mso mso = new Mso("9x9", "intro", "contactEmail", Mso.TYPE_NN);
+        mso.setId(msoId);
+        
+        // stubs
+        when(mockMsoMngr.getByNameFromCache(anyString())).thenReturn(mso);
+        
+        PowerMockito.mockStatic(MsoManager.class);
+        when(MsoManager.isNNMso((Mso) anyObject())).thenReturn(true);
+        
+        when(mockMsoMngr.findOneByName(anyString())).thenReturn(mso);
+        
+        PowerMockito.mockStatic(FacebookLib.class);
+        when(FacebookLib.getDialogOAuthPath(anyString(), anyString(), (Mso) anyObject())).thenCallRealMethod();
+        
+        // execute
         String result = playerAPI.fbLogin(req);
+        
+        // verify
+        verify(mockMsoMngr).getByNameFromCache(anyString());
+        
+        PowerMockito.verifyStatic();
+        MsoManager.isNNMso(mso);
+        
+        verify(mockMsoMngr).findOneByName(null);
+        
+        final String protocol = "http://";
+        final String domain = "www.localhost"; // if wrong here, look ApiContext implement
+        final String path = "/fb/login";
+        PowerMockito.verifyStatic();
+        FacebookLib.getDialogOAuthPath(referrer, protocol + domain + path, mso);
+        
         assertTrue(
                 "The url redirection string slould start with 'redirect:'",
                 result.matches("^redirect:.*$"));
