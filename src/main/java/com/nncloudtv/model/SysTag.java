43da2688aff7a7dfdc99ef025e5633df60fb248a
Louis Jeng <louisje@gmail.com>
2014-07-29 11:48:47 +0800

    clean up

--- src/main/java/com/nncloudtv/model/SysTag.java
+++ src/main/java/com/nncloudtv/model/SysTag.java
@@ -8,16 +8,18 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
+import com.nncloudtv.lib.NnStringUtil;
+
 @PersistenceCapable(table = "systag", detachable = "true")
 public class SysTag {
     
     @PrimaryKey
     @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-    private long id;
-
+    long id;
+    
     @Persistent
     protected long msoId;
-
+    
     @Persistent
     private short type;
     public static final short TYPE_CATEGORY     = 1;
@@ -29,124 +31,124 @@ public class SysTag {
     public static final short TYPE_33SET        = 7;
     public static final short TYPE_DESTROYED    = 8;
     public static final short TYPE_WHATSON      = 9;
-
+    
     @Persistent
     protected short sorting; // indicate how to sort SysTagMaps that belong to this SysTag
     public static final short SORT_SEQ = 1; //default
     public static final short SORT_DATE = 2;
     
     @Persistent
-    private short seq;
+    short seq;
     
     @Persistent
-    private boolean featured; //for set
-
+    boolean featured; //for set
+    
     @Persistent
-    private short timeStart; //for dayparting
-
+    short timeStart; //for dayparting
+    
     @Persistent
-    private short timeEnd; //for dayparting
-
+    short timeEnd; //for dayparting
+    
     @Persistent
-    @Column(jdbcType="VARCHAR", length=10)    
-    private String attr;
-    public static final short ATTR_APP_STACK = 0;
+    @Column(jdbcType = NnStringUtil.VARCHAR, length = NnStringUtil.VERY_SHORT_STRING_LENGTH)
+    String attr;
+    public static final short ATTR_APP_STACK        = 0;
     public static final short ATTR_APP_SUBSCRIPTION = 1;
-    public static final short ATTR_APP_ACCOUNT = 2;
-    public static final short ATTR_APP_CHANNEL = 3;
-    public static final short ATTR_APP_DIR = 4;
-    public static final short ATTR_APP_SEARCH = 5;
+    public static final short ATTR_APP_ACCOUNT      = 2;
+    public static final short ATTR_APP_CHANNEL      = 3;
+    public static final short ATTR_APP_DIR          = 4;
+    public static final short ATTR_APP_SEARCH       = 5;
     
     @Persistent 
-    private Date createDate;
+    Date createDate;
         
     @Persistent
-    private Date updateDate;
+    Date updateDate;
     
     public long getId() {
         return id;
     }
-
+    
     public void setId(long id) {
         this.id = id;
     }
-
+    
     public long getMsoId() {
         return msoId;
     }
-
+    
     public void setMsoId(long msoId) {
         this.msoId = msoId;
     }
-
+    
     public short getType() {
         return type;
     }
-
+    
     public void setType(short type) {
         this.type = type;
     }
-
+    
     public short getSeq() {
         return seq;
     }
-
+    
     public void setSeq(short seq) {
         this.seq = seq;
     }
-
+    
     public Date getCreateDate() {
         return createDate;
     }
-
+    
     public void setCreateDate(Date createDate) {
         this.createDate = createDate;
     }
-
+    
     public Date getUpdateDate() {
         return updateDate;
     }
-
+    
     public void setUpdateDate(Date updateDate) {
         this.updateDate = updateDate;
     }
-
+    
     public boolean isFeatured() {
         return featured;
     }
-
+    
     public void setFeatured(boolean featured) {
         this.featured = featured;
     }
-
+    
     public short getTimeStart() {
         return timeStart;
     }
-
+    
     public void setTimeStart(short timeStart) {
         this.timeStart = timeStart;
     }
-
+    
     public short getTimeEnd() {
         return timeEnd;
     }
-
+    
     public void setTimeEnd(short timeEnd) {
         this.timeEnd = timeEnd;
     }
-
+    
     public String getAttr() {
         return attr;
     }
-
+    
     public void setAttr(String attr) {
         this.attr = attr;
     }
-
+    
     public short getSorting() {
         return sorting;
     }
-
+    
     public void setSorting(short sorting) {
         this.sorting = sorting;
     }
