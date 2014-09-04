edaab6a43652a02681eae664e75aa4952814cfa8
Louis Jeng <louisje@gmail.com>
2014-09-04 11:07:06 +0800

    trim

--- src/main/java/com/nncloudtv/web/api/ApiMisc.java
+++ src/main/java/com/nncloudtv/web/api/ApiMisc.java
@@ -23,7 +23,6 @@ import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
