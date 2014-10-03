973c86763e39b3450904e1d05d94d2294b6e9905
Louis Jeng <louisje@gmail.com>
2014-10-03 17:12:45 +0800

    polish

--- src/main/java/com/nncloudtv/task/PipingTask.java
+++ src/main/java/com/nncloudtv/task/PipingTask.java
@@ -72,18 +72,22 @@ public class PipingTask extends Thread {
                     out.write(buf, 0, len);
                     
                     // progress log
-                    if (total % 13 == 0) {
+                    if (total % 13 == 0 ) {
                         
                         Date now = NnDateUtil.now();
                         long deltaSec = (now.getTime() - lastTime.getTime());
-                        long deltaLen = total - lastTotal;
-                        float pipingSpeed = (float) deltaLen / deltaSec;
-                        float avarageSpeed = (float) total / (now.getTime() - startTime.getTime());
                         
-                        log.info("piping speed = " + pipingSpeed + " kbps, avarage = " + avarageSpeed + " kbps");
-                        
-                        lastTime = now;
-                        lastTotal = total;
+                        if (deltaSec > 1000) {
+                            
+                            long deltaLen = total - lastTotal;
+                            float pipingSpeed = (float) deltaLen / deltaSec;
+                            float avarageSpeed = (float) total / (now.getTime() - startTime.getTime());
+                            
+                            log.info("piping speed = " + pipingSpeed + " kbps, avarage = " + avarageSpeed + " kbps");
+                            
+                            lastTime = now;
+                            lastTotal = total;
+                        }
                     }
                 }
                 
