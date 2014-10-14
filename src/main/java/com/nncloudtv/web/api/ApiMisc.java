f9de926cc62c704e2472e82b7b7e6c4ae722bce3
Louis Jeng <louisje@gmail.com>
2014-10-14 19:43:54 +0800

    log

--- src/main/java/com/nncloudtv/web/api/ApiMisc.java
+++ src/main/java/com/nncloudtv/web/api/ApiMisc.java
@@ -551,7 +551,7 @@ public class ApiMisc extends ApiGeneric {
             
         } catch (IOException e) {
             
-            log.info(e.getMessage());
+            log.warning(e.getMessage());
             internalError(resp);
             return;
         }
