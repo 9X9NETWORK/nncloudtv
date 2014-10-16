6c33fbcc58a9c44c188a522452a0ab249db170d4
Louis Jeng <louisje@gmail.com>
2014-10-16 15:20:35 +0800

    NnItem.status

--- src/main/java/com/nncloudtv/model/NnItem.java
+++ src/main/java/com/nncloudtv/model/NnItem.java
@@ -44,8 +44,8 @@ public class NnItem {
     public static final short ACTIVE     = 0;
     public static final short INACTIVE   = 1;
     public static final short PROCESSING = 2;
-    public static final short WAIT_FOR_APPROVAL = 3;
-    public static final short REJECTED   = 4;
+    public static final short REJECTED   = 3;
+    public static final short WAIT_FOR_APPROVAL = 4;
     public static final short TERMINATED = 5;
     
     public NnItem(long msoId, long channelId, short billingPlatform) {
