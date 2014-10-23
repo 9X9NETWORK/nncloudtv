31effcdc2b2aa518c11c81e9af30f055a902e968
Louis Jeng <louisje@gmail.com>
2014-10-23 15:34:19 +0800

    nnprogram.contentType

--- src/main/java/com/nncloudtv/lib/NnNetUtil.java
+++ src/main/java/com/nncloudtv/lib/NnNetUtil.java
@@ -17,7 +17,6 @@ import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
