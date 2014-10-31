cbe9931e19567707b98bba29e233940e7f11bf6e
Louis Jeng <louisje@gmail.com>
2014-10-31 16:57:28 +0800

    Aspect

---
+++ src/main/java/com/nncloudtv/aspect/LoggingAspect.java
@@ -0,0 +1,16 @@
+package com.nncloudtv.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+    
+    @Before("execution(* com.nncloudtv.web.api.echo(..))")
+    public void logApiEcho(JoinPoint joinPoint) {
+        
+        System.out.println("[aspect] " + joinPoint.getSignature().getName());
+        
+    }
+}
