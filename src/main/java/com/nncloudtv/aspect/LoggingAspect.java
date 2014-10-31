afa92f96c0d57b5b6bd585f9d11e22250f4fa095
Louis Jeng <louisje@gmail.com>
2014-10-31 18:31:32 +0800

    config

--- src/main/java/com/nncloudtv/aspect/LoggingAspect.java
+++ src/main/java/com/nncloudtv/aspect/LoggingAspect.java
@@ -3,10 +3,14 @@ package com.nncloudtv.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Configuration
+@EnableAspectJAutoProxy
 public class LoggingAspect {
     
     @After("com.nncloudtv.web.api.ApiContext.isProductionSite(..)")
