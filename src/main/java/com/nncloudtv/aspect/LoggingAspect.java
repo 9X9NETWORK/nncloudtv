5be85778c7e47f0ceabb887afd75c7d435d580b2
Louis Jeng <louisje@gmail.com>
2014-10-31 17:05:46 +0800

    apect

--- src/main/java/com/nncloudtv/aspect/LoggingAspect.java
+++ src/main/java/com/nncloudtv/aspect/LoggingAspect.java
@@ -7,7 +7,7 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
     
-    @Before("execution(* com.nncloudtv.web.api.echo(..))")
+    @Before("execution(* com.nncloudtv.web.api.ApiMisc.echo(..))")
     public void logApiEcho(JoinPoint joinPoint) {
         
         System.out.println("[aspect] " + joinPoint.getSignature().getName());
