2c529240021ffef611d0c55f2a05fec102930aee
Louis Jeng <louisje@gmail.com>
2014-11-14 23:50:21 +0800

    synchronized

--- src/main/java/com/nncloudtv/task/ScheduledTask.java
+++ src/main/java/com/nncloudtv/task/ScheduledTask.java
@@ -3,24 +3,22 @@ package com.nncloudtv.task;
 import java.util.logging.Logger;
 
 import org.apache.commons.io.FileUtils;
-import org.springframework.stereotype.Service;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import com.nncloudtv.lib.CacheFactory;
 import com.nncloudtv.lib.NnDateUtil;
 
-@Service
 @EnableScheduling
 public class ScheduledTask {
     
     protected static Logger log = Logger.getLogger(ScheduledTask.class.getName());
     
-    public final int MC_INTERVAL = 584141; // memcache check interval (milliseconds)
-    public final int GC_INTERVAL = 604171; // garbage collection interval (milliseconds)
+    static final int MC_INTERVAL = 584141; // memcache check interval (milliseconds)
+    static final int GC_INTERVAL = 604171; // garbage collection interval (milliseconds)
     
     @Scheduled(fixedRate = GC_INTERVAL)
-    public void triggerGC() {
+    synchronized public void triggerGC() {
         
         System.gc(); // trigger garbage collection
         
@@ -37,7 +35,7 @@ public class ScheduledTask {
     }
     
     @Scheduled(fixedDelay = MC_INTERVAL)
-    public void checkingMemcacheServer() {
+    synchronized public void checkingMemcacheServer() {
         
         if (CacheFactory.isEnabled) {
             
