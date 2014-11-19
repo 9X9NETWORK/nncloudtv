c491a4230d40cc77a9a335305ba11d3f1c9a0bb3
Louis Jeng <louisje@gmail.com>
2014-11-19 11:33:37 +0800

    fix

--- src/main/java/com/nncloudtv/task/CounterTask.java
+++ src/main/java/com/nncloudtv/task/CounterTask.java
@@ -1,5 +1,6 @@
 package com.nncloudtv.task;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -18,10 +19,14 @@ public class CounterTask extends CounterFactory {
     
     @Scheduled(fixedDelay = CC_INTERVAL)
     public void cleanDirtyCounter() {
+        
         synchronized (dirtyCounters) {
-            Set<Entry<String,Integer>> entrySet = dirtyCounters.entrySet();
-            Iterator<Entry<String, Integer>> it = entrySet.iterator();
+            
+            HashSet<Entry<String, Integer>> counterSet = new HashSet<Entry<String,Integer>>(dirtyCounters.entrySet());
+            dirtyCounters.clear();
+            Iterator<Entry<String, Integer>> it = counterSet.iterator();
             while (it.hasNext()) {
+                
                 Entry<String, Integer> entry = it.next();
                 increment(entry.getKey(), entry.getValue());
                 System.out.println(String.format("[counter] \"%s\" increment %d", entry.getKey(), entry.getValue()));
