7c14fa669d0e88b9d26a842b0d3cf6d3a0374547
Louis Jeng <louisje@gmail.com>
2014-11-25 15:24:38 +0800

    PersistenceCapable

--- src/main/java/com/nncloudtv/model/PersistentModel.java
+++ src/main/java/com/nncloudtv/model/PersistentModel.java
@@ -1,12 +1,9 @@
 package com.nncloudtv.model;
 
 import javax.jdo.annotations.IdGeneratorStrategy;
-import javax.jdo.annotations.IdentityType;
-import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
-@PersistenceCapable(detachable = "true", identityType = IdentityType.APPLICATION)
 public class PersistentModel {
     
     @PrimaryKey
