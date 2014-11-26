9e35be7a52116d2e79d8a650989fbd2d8135483f
Louis Jeng <louisje@gmail.com>
2014-11-26 18:45:43 +0800

    I rewrote whole CounterFactory, so take Google license away.

--- src/main/java/com/nncloudtv/service/CounterFactory.java
+++ src/main/java/com/nncloudtv/service/CounterFactory.java
@@ -1,18 +1,3 @@
-/* Copyright (c) 2009 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.nncloudtv.service;
 
 import java.util.HashMap;
@@ -26,7 +11,14 @@ import com.nncloudtv.model.Counter;
 import com.nncloudtv.model.CounterShard;
 
 /**
- * Finds or creates a sharded counter with the desired name.
+ * A counter which can be incremented rapidly.
+ *
+ * Capable of incrementing the counter and increasing the number of shards.
+ * When incrementing, a random shard is selected to prevent a single shard
+ * from being written to too frequently. If increments are being made too
+ * quickly, increase the number of shards to divide the load. Performs
+ * datastore operations using JDO.
+ *
  */
 public class CounterFactory {
     
@@ -92,13 +84,14 @@ public class CounterFactory {
              * Sharding Formula
              * 
              * To estimate how many shards needed
+             * and auto adding shards as well 
              * 
              *      0 ~   9999: 1 shard
              *  10000 ~  39999: 2 shards
              *  40000 ~  89999: 3 shards
              *  90000 ~ 159999: 4 shards
              * 160000 ~ 249999: 5 shards
-             * ... etc
+             * ... etc ...
              * 
              * @author Louis Jeng <louis.jeng@flipr.tv>
              */
