c5ecacc9ef2878cdf948ef41e160799dafc9c9a0
Louis Jeng <louisje@gmail.com>
2014-12-02 15:48:59 +0800

    config

--- installer/installer.sh
+++ installer/installer.sh
@@ -3,7 +3,6 @@
 # installer.sh - build & deploy for develpement/test site
 #
 
-jetty="jetty"
 script_path=`readlink -f "$0"`
 script_dir=`dirname "$script_path"`
 
@@ -16,28 +15,13 @@ echo " **    9x9 DevOps Installer  **"
 echo "   **                          *"
 echo "     *****************************"
 echo
-test "$1" != "-n" \
-    && echo -n "Checking your sudo permission ... " \
-    && sudo id \
-    && if test $? -eq 1; then echo "failed."; exit; fi \
-    && echo
 
 cd ..
 
-if test "$1" = "-x"; then # speed up
-    mvn -Dmaven.test.skip=true compile war:war \
-    install:install-file -Dfile=./lib/CcxClientApi.jar -DgroupId=com.clearcommerce -DartifactId=clear-commerce -Dversion=5.10.0.3706 -Dpackaging=jar \
-    && sudo cp -v target/root.war /usr/share/$jetty/webapps/root.war \
-    && sudo service $jetty restart
-    exit
-fi
-
-mvn -Dmaven.test.skip=true clean\
+mvn -Dmaven.test.skip=true \
     install:install-file -Dfile=./lib/CcxClientApi.jar -DgroupId=com.clearcommerce -DartifactId=clear-commerce -Dversion=5.10.0.3706 -Dpackaging=jar \
     compile \
     datanucleus:enhance \
     install war:war \
-&& test "$1" != "-n" \
-&& sudo cp -v target/root.war /usr/share/$jetty/webapps/root.war \
-&& (sudo service $jetty restart; sudo service memcached restart)
+&& cp -v target/root.war $JETTY_HOME/webapps/ && jetty.sh restart
 
