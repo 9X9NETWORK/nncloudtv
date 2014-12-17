ebc4da3f077f457d6f622dd5da62bae683e56b0c
Louis Jeng <louisje@gmail.com>
2014-12-17 18:15:14 +0800

    remove message array

--- src/main/java/com/nncloudtv/task/MessageQueueTask.java
+++ src/main/java/com/nncloudtv/task/MessageQueueTask.java
@@ -1,17 +1,15 @@
 package com.nncloudtv.task;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.nncloudtv.lib.NnDateUtil;
 import com.nncloudtv.lib.QueueFactory;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConsumerCancelledException;
 import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.ShutdownSignalException;
@@ -21,36 +19,26 @@ import com.rabbitmq.client.ShutdownSignalException;
 public class MessageQueueTask extends QueueFactory implements ScheduledTask {
     
     protected static Logger log = Logger.getLogger(MessageQueueTask.class.getName());
-    static List<String> messages = new ArrayList<String>();
-    final static int MESSAGE_ARRAY_SIZE = 50;
-    
-    static Channel rabbit = null;
     
     @Scheduled(fixedRate = MQ_INTERVAL)
     synchronized public static void receiveMessage() {
         
         try {
-            long born = NnDateUtil.timestamp();
-            if (rabbit == null || rabbit.isOpen() == false)
-                rabbit = getConnection().createChannel();
+            Connection connection = getConnection();
+            Channel rabbit = connection.createChannel();
             String queueName = rabbit.queueDeclare().getQueue();
             System.out.println("[mq] queueName = " + queueName);
             rabbit.queueBind(queueName, MESSAGE_EXCHANGE, "");
             QueueingConsumer consumer = new QueueingConsumer(rabbit);
             rabbit.basicConsume(queueName, consumer);
-            
-            while(NnDateUtil.timestamp() - born < MQ_INTERVAL) {
-                
+            while(true) {
                 QueueingConsumer.Delivery delivery = consumer.nextDelivery(MQ_INTERVAL);
                 if (delivery == null) break;
                 String message = new String(delivery.getBody());
-                if (!messages.contains(message)) {
-                    
-                    messages.add(message);
-                    System.out.println(String.format((char)27 + "[2;36m[mq]" + (char)27 + "[0m received {%s}", message));
-                }
+                System.out.println(String.format((char)27 + "[2;36m[mq]" + (char)27 + "[0m received {%s}", message));
             }
             rabbit.close();
+            connection.close();
             
         } catch (IOException e) {
             
@@ -75,9 +63,6 @@ public class MessageQueueTask extends QueueFactory implements ScheduledTask {
         } finally {
             
             System.out.println("[mq] finished");
-            int fromIndex = messages.size() - MESSAGE_ARRAY_SIZE;
-            if (fromIndex < 0) fromIndex = 0;
-            messages = messages.subList(fromIndex, messages.size());
         }
     }
     
